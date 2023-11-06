e6dbb0f51e8209814b0c7f701c4c02afd0ec03a7
UJIN901 <dalvit9655@gmail.com>
2023-11-06 14:47:17 +0900

    add step 5

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -8,6 +8,7 @@ public class App {
     Scanner scanner = new Scanner(System.in);
     List<Quotation> quotations = new ArrayList<>();
     int quoteId = 0;
+
     void run() {
         System.out.println("== 명언 앱 ==");
         while (true) {
@@ -17,6 +18,8 @@ public class App {
                 break;
             } else if (cmd.equals("등록")) {
                 registerQuote();
+            } else if (cmd.equals("목록")) {
+                displayQuote();
             }
         }
     }
@@ -32,5 +35,17 @@ public class App {
         quotations.add(quotation);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n", quoteId);
+    }
+    void displayQuote() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        if (quotations.isEmpty()) {
+            System.out.println("등록된 명언이 없습니다.");
+        } else {
+            for (int i = quotations.size() - 1; i >= 0; i--) {
+                Quotation quoteBit = quotations.get(i);
+                System.out.printf("%d / %s / %s\n", quoteBit.id, quoteBit.author, quoteBit.text);
+            }
+        }
     }
 }

8b4b1a51e2d9253e1699d7fd4f589b6c08cce1a3
UJIN901 <dalvit9655@gmail.com>
2023-11-06 11:45:43 +0900

    add step 2

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,18 +1,32 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    void run(){
-        Scanner scanner = new Scanner(System.in);
-
+    Scanner scanner = new Scanner(System.in);
+    List<Quotation> quotations = new ArrayList<>();
+    void run() {
         System.out.println("== 명언 앱 ==");
-        while(true){
+        while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
-            if(cmd.equals("종료")){
+            if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                registerQuote();
             }
         }
+    }
+
+    void registerQuote() {
+        System.out.print("명언 : ");
+        String cmdText = scanner.nextLine();
+        System.out.print("작가 : ");
+        String cmdAuthor = scanner.nextLine();
+
+        Quotation quotation = new Quotation(cmdText, cmdAuthor);
+        quotations.add(quotation);
     }
 }
---
+++ src/main/java/com/ll/Quotation.java
@@ -0,0 +1,10 @@
+package com.ll;
+
+public class Quotation {
+    String text;
+    String author;
+    Quotation(String text, String author){
+        this.text = text;
+        this.author = author;
+    }
+}
